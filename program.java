32e72be02f1e2e9130232565089e3107a623f095
christober14 <168295805+christober14@users.noreply.github.com>
2025-07-30 14:50:07 +0530

    Updated by User 2

--- program.java
+++ program.java
@@ -1,8 +1,11 @@
-public class HelloWorld {
+public class program {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
 
         System.out.println("A method changed a simple method");
+    }
+    public void Practice{
+    System.out.println("Hello");
     }
 }
  
